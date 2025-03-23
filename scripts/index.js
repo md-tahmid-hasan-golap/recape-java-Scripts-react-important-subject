ca3073c94c054941d31bf1b2066d0be53f1fbe7c
golap-raj <golapraj47@gmail.com>
2025-03-24 00:48:51 +0600

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -39,3 +39,63 @@
 // else{
 //     console.log("Noooooooooooo")
 // }
+
+
+
+// amra condition er moddhe or and used korte  pari || ,, &&
+
+
+// Array diclaration
+
+// const friends = ["Golap", "Sabbir", "Basar", "Imrul"]
+// console.log(friends)
+// console.log(friends.slice(1, 2))
+// console.log(friends.splice(1,2))
+// friends.pop()
+// friends.push("Raj")
+
+// friends.unshift("go")
+// friends.shift()
+// console.log(friends)
+
+
+// console.log(friends[0])
+
+// console.log(friends[1])
+
+// console.log(friends[2])
+
+
+
+// ai ber loop niye kotha hobe 
+
+// 3 dhoroner loop amra used kori 
+
+// 1.. for of loop;
+
+// 2.. for loop,
+// 3.. while loop
+
+
+// const numbers = [12, 33, 55, 22, 87, 90]
+// // console.log(numbers);
+// // for(let number of numbers){
+// //     console.log(number)
+// // }
+
+// for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//     console.log(element)
+    
+// }
+
+
+// ai ber function diclaration , 2 vabe function diclar kora jai
+
+function sum (a, b) {
+    const result = a + b;
+    console.log(result)
+
+}
+
+sum(10, 5)
