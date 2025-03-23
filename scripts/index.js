ac26b82a6cbb31b7406cf4e52b9ad72e42ddd7c3
golap-raj <golapraj47@gmail.com>
2025-03-24 00:17:54 +0600

    resolve

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+// variable 3 prokar 
+
+// var , let , const
+
+// var --- ja ta amra kokhono used korbo nha
+
+// let & const ata diye amra akhon thake variable diclar korbo
+
+// let ata amra used korbo jokhon amra kunu man change korbo 
+
+// let name = "Golap"
+// name = "Tahmid"
+// console.log(name)
+
+
+// const ata amra used korbo jokhon amra kunu man change korbo nha
+
+// const countryName = "Bangladesh";
+// countryName = "BD"
+// console.log(countryName)
+
+
+
+// if else codition 
+
+// const number = 20;
+
+// if(number > 20){
+//     console.log("YES")
+// }
+// else{
+//     console.log("Noooooooooooo")
+// }
+// const number = 20;
+
+// if(number >= 20){
+//     console.log("YES")
+// }
+// else{
+//     console.log("Noooooooooooo")
+// }
