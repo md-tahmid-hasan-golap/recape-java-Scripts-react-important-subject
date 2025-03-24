93f1bed4c602e44f689de989e322650e58e375ec
golap-raj <golapraj47@gmail.com>
2025-03-24 23:53:13 +0600

    add sqared oparator

--- scripts/index.js
+++ scripts/index.js
@@ -92,10 +92,87 @@
 
 // ai ber function diclaration , 2 vabe function diclar kora jai
 
-function sum (a, b) {
-    const result = a + b;
-    console.log(result);
+// function sum (a, b) {
+//     const result = a + b;
+//     console.log(result);
 
-}
+// }
 
-sum(10, 5);
+// sum(10, 5);
+
+
+// tamplate String
+
+// const fName = "Golap";
+// const lName = "Raj"
+// console.log(fName, lName)
+// console.log(fName + " " + lName)
+
+// const fullName = fName + " " + lName;
+// console.log(fullName)
+
+// console.log(`${fName} ${lName}`)
+// console.log(`My Name Is ${fName} ${lName}`)
+
+
+// * --- function 2 vabe used kore ;
+
+// 1.. regular function;
+// 2.. arroow function
+
+// regular function diclaration 
+
+// function sum (a,b) {
+//     const result = a + b;
+//     return result
+
+// }
+
+// const output = sum(12, 33);
+// console.log(output)
+
+
+
+// arrow function diclaration
+
+// const sum = (a, b,c) => {
+//     const result = a + b +c
+//     return result
+
+// }
+
+// const output = sum(12, 22, 33);
+// console.log(output)
+
+
+// spred oparator ...sum
+
+// const numbers = [12, 33, 65, 88, 90];
+// console.log(Math.max(...numbers))
+
+// const newNumbers = [100, 2000, ...numbers];
+// console.log(newNumbers)
+
+// const newArray = [...numbers, 1, 2];
+// console.log(newArray)
+
+
+
+// ai ber amra recape korbo  Array --- forEach, map, find, filter
+
+const products = [
+    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+    {Namen : "Sam", Brand : "SamSung", price : 4000, color : "white"},
+    {Namen : "Apple1", Brand : "Apple", price : 5000, color : "gold"},
+    {Namen : "Apple2", Brand : "Apple", price : 5000, color : "gold"},
+    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+    {Namen : "Redmi2", Brand : "xaiomi", price : 3000, color : "Black"},
+]
+
+
+
+// console.log(products)
+
+// for(let product of products){
+//     console.log(product)
+// }
