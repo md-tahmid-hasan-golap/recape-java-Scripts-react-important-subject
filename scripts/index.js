0dbea6d5c1257e1456d8029d378843f3b03cf66c
golap-raj <golapraj47@gmail.com>
2025-03-26 13:44:30 +0600

    resolve

--- scripts/index.js
+++ scripts/index.js
@@ -160,21 +160,21 @@
 
 // ai ber amra recape korbo  Array --- forEach, map, find, filter
 
-const products = [
-    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
-    {Namen : "Sam", Brand : "SamSung", price : 4000, color : "white"},
-    {Namen : "Apple1", Brand : "Apple", price : 5000, color : "gold"},
-    {Namen : "Apple2", Brand : "Apple", price : 5000, color : "gold"},
-    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
-    {Namen : "Redmi2", Brand : "xaiomi", price : 3000, color : "Black"},
-]
+// const products = [
+//     {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+//     {Namen : "Sam", Brand : "SamSung", price : 4000, color : "white"},
+//     {Namen : "Apple1", Brand : "Apple", price : 5000, color : "gold"},
+//     {Namen : "Apple2", Brand : "Apple", price : 5000, color : "gold"},
+//     {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+//     {Namen : "Redmi2", Brand : "xaiomi", price : 3000, color : "Black"},
+// ]
 
 // filter
 // const result = products.filter(product => product.Brand === "Apple");
 // console.log (result)
-const result = products.find(product => product.price > 4000);
-console.log (result)
-// products.forEach(product => console.log(product))
+// const result = products.find(product => product.price > 4000);
+// console.log (result)
+// // products.forEach(product => console.log(product))
 // const result = products.map(product => product.price)
 // console.log(result)
 
@@ -190,3 +190,139 @@ console.log (result)
 // for(let product of products){
 //     console.log(product)
 // }
+
+
+// Array dictruct charing
+
+// const friends = ["golap", "tapos", "imrul", "raj"];
+
+// const [first, secend, third , fourth] = friends;
+// console.log(first)
+// console.log(secend)
+// const name = fourth;
+// console.log(name)
+// console.log(friends)
+// console.log(friends[0])
+
+// console.log(friends[2])
+
+
+
+
+
+
+// Object dictruct charing
+
+// const person = {
+//     name : "golap",
+//     id : 10,
+//     fatherName : "abdullah",
+//     motherName : "chamely"
+// }
+
+// console.log(person.id)
+// console.log(person["fatherName"])
+
+// const {name , id , fatherName, motherName} = person;
+// console.log(name);
+// console.log(motherName)
+
+
+
+// JSON conver of Object
+
+
+// const person = {
+//     name : "golap",
+//     id : 10,
+//     fatherName : "abdullah",
+//     motherName : "chamely",
+//      friends : ["golap", "tapos", "imrul", "raj"],
+
+
+// }
+
+// // console.log(person)
+
+// const jsonData = JSON.stringify(person);
+// console.log(jsonData)
+
+// const nojson = JSON.parse(jsonData);
+// console.log(nojson)
+
+
+
+// fetch used
+
+// fetch ("url")
+// .then((res) => res.json())
+// .then((data) => console.log(data))
+
+
+
+// Object kay and value ber kora
+
+
+
+// const person = {
+//     name : "golap",
+//     id : 10,
+//     fatherName : "abdullah",
+//     motherName : "chamely",
+//      friends : ["golap", "tapos", "imrul", "raj"],
+
+
+// }
+
+// const key = Object.keys(person);
+// console.log(key)
+
+// const value = Object.values(person);
+// console.log(value)
+
+
+// console.log(Object.keys(person))
+// console.log(Object.values(person))
+
+
+// Object er upor for in loop 
+
+// const person = {
+//     name : "golap",
+//     id : 10,
+//     fatherName : "abdullah",
+//     motherName : "chamely",
+//      friends : ["golap", "tapos", "imrul", "raj"],
+
+
+// }
+
+
+// for(let per in person){
+//     console.log(per)
+// }
+
+
+
+
+
+// Array of Object
+
+
+const products = [
+    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+    {Namen : "Sam", Brand : "SamSung", price : 4000, color : "white"},
+    {Namen : "Apple1", Brand : "Apple", price : 5000, color : "gold"},
+    {Namen : "Apple2", Brand : "Apple", price : 5000, color : "gold"},
+    {Namen : "Redmi", Brand : "xaiomi", price : 3000, color : "Black"},
+    {Namen : "Redmi2", Brand : "xaiomi", price : 3000, color : "Black"},
+]
+
+const newArray = {
+    name : "golap",
+    id : 12,
+
+}
+
+const newArray2 = [...products,newArray];
+console.log(newArray2)
